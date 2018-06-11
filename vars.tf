c03be1fd78f26c71b0f68d0b3f23e45e9ca784ea
Wael Ben Salah <wael.ben.salah@gmail.com>
2018-06-11 13:50:21 +0200

    Change virtualisation type

--- vars.tf
+++ vars.tf
@@ -19,8 +19,9 @@ variable "AMIS" {
   type = "map"
   default = {
     us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
+    us-east-2 = "ami-98023efd"
     eu-west-1 = "ami-844e0bf7"
+    eu-west-2 = "ami-5daa463a"
     eu-west-3 = "ami-ad53e2d0"
   }
 }
