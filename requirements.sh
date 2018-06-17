37e352bc14be7f93bc00e5c8e7ce781b8691daf4
Wael Ben Salah <wael.ben.salah@gmail.com>
2018-06-18 01:47:18 +0200

    edit npm package

--- requirements.sh
+++ requirements.sh
@@ -42,4 +42,6 @@ curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 npm install -g @angular/cli@latest
-
+sudo apt-get update
+sudo apt-get install - y ghostscript
+sudo apt-get install -y imagemagick
